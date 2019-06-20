f3ec9fd7a632dd39207a9263705dc30c762c0adf
SANDBOB <sakul.s2557@gmail.com>
2019-06-20 18:23:00 +0700

    RecyclerView HORIZONTAL

--- app/src/main/java/com/example/application_job/carousels/InfiniteCarouselActivity.kt
+++ app/src/main/java/com/example/application_job/carousels/InfiniteCarouselActivity.kt
@@ -22,14 +22,8 @@ import com.yarolegovich.discretescrollview.InfiniteScrollAdapter
 import kotlinx.android.synthetic.main.activity_infinite_scroll_carousel.*
 
 
-class InfiniteCarouselActivity : AppCompatActivity(), DiscreteScrollView.OnItemChangedListener<MyAdapter.MyViewHolder>
+class InfiniteCarouselActivity : AppCompatActivity()
     {
-    override fun onCurrentItemChanged(viewHolder: MyAdapter.MyViewHolder?, adapterPosition: Int) {
-        val realPosition = mInfiniteScrollWrapper.realCurrentPosition
-        log("onCurrentItemChanged $realPosition")
-    }
-
-    private lateinit var mInfiniteScrollWrapper: InfiniteScrollAdapter<*>
     private lateinit var mAdapter: MyAdapter
     private var arrayList: ArrayList<MyItem> = arrayListOf()
 
@@ -38,69 +32,35 @@ class InfiniteCarouselActivity : AppCompatActivity(), DiscreteScrollView.OnItemC
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infinite_scroll_carousel)
 
-
-//        var dataViewModel: DataViewModel = ViewModelProviders.of(this).get(DataViewModel::class.java)
-//        dataViewModel.getArrayList().observe(this, Observer {
-//            mAdapter = MyAdapter(it!!)
-//            recycler.layoutManager = CenterZoomLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
-//            recycler.adapter = MyAdapter(it)
-//
-//            val snapHelper: SnapHelper = LinearSnapHelper()
-//            snapHelper.attachToRecyclerView(recycler)
-//
-//            pageIndicator.attachTo(recycler)
-//            indicator.attachToRecyclerView(recycler)
-//            recycler.addItemDecoration(CirclePagerIndicatorDecoration())
-//
-//        })
-
         mAdapter = MyAdapter(arrayList)
-
-
-        //DiscreteScrollView--------------------------------------------------------------------------------//
-        // Infinite scroll
-        mInfiniteScrollWrapper = InfiniteScrollAdapter.wrap(mAdapter)
-        infinite_carousel_1.adapter = mInfiniteScrollWrapper
-
-        // Item transformer
-        infinite_carousel_1.setItemTransformer(InfiniteCarouselTransformer())
-
-        // Item change listener
-        infinite_carousel_1.addOnItemChangedListener(this)
-        //DiscreteScrollView--------------------------------------------------------------------------------//
-
         // data
         loadData()
 
 
         //RecyclerView-------------------------------------------------------------------------------------------------//
-//        recycler.layoutManager = CenterZoomLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
-//        recycler.adapter = MyAdapter(arrayList)
+        recycler.layoutManager = CenterZoomLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
+        recycler.adapter = MyAdapter(arrayList)
 
-//        val snapHelper: SnapHelper = LinearSnapHelper()
-//        snapHelper.attachToRecyclerView(recycler)
+        recycler2.layoutManager = CenterZoomLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
+        recycler2.adapter = MyAdapter(arrayList)
+
+
+
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recycler)
+        snapHelper.attachToRecyclerView(recycler2)
         //------------------------------------------------------------------------------------------------------------//
 
 
         //RecyclerView Indicator Library----------------------------------------------------------------------//
-//        pageIndicator.attachTo(recycler)
-//        indicator.attachToRecyclerView(recycler)
-//        recycler.addItemDecoration(CirclePagerIndicatorDecoration())
+        pageIndicator.attachTo(recycler)
+        indicator.attachToRecyclerView(recycler)
+        recycler.addItemDecoration(CirclePagerIndicatorDecoration())
+
+        pageIndicator2.attachTo(recycler2)
+        indicator2.attachToRecyclerView(recycler2)
+        recycler2.addItemDecoration(CirclePagerIndicatorDecoration())
         //----------------------------------------------------------------------------------------------------//
-
-        //DiscreteScrollViewIndicator Indicator use DiscreteScrollViewIndacator.java "No Complete"
-
-        val DP = Resources.getSystem().displayMetrics.density
-        val pageIndicators = DiscreteScrollViewIndicator.Builder(infinite_carousel_1)
-            .setColorActive(rgb(0,0,0))
-            .setColorInactive(rgb(255,0,0))
-            .setIndicatorStrokeWidth(4 * DP)
-            .setIndicatorItemPadding(10 * DP)
-            .align(DiscreteScrollViewIndicator.Alignment.PARENT_TOP)
-            .matchParentWidth()
-        infinite_carousel_2.addItemDecoration(pageIndicators)
-
-        //-----------------------------------------------------------------------------------------------------
 
     }
     val all = arrayOf("https://dummyimage.com/250x500/000/fff",
@@ -111,15 +71,9 @@ class InfiniteCarouselActivity : AppCompatActivity(), DiscreteScrollView.OnItemC
         "https://dummyimage.com/600x400/000/fff")
 
     private fun loadData() {
-        log("Loading data...")
         for (i in all.indices) {
             arrayList.add(MyItem("Title ${i+1}", "${all[i]}"))
         }
         mAdapter.notifyDataSetChanged()
     }
-
-    private fun log(message: String) {
-        Log.d("BackgroundToForeground", message)
-    }
-
 }
